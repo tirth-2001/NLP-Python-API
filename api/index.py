4a4a3788842750c326b0c465b0f79eb0a4bca775
tirth-2001 <tirthgpatel.27@gmail.com>
2021-09-07 13:40:27 +0530

    Second Function

--- api/index.py
+++ api/index.py
@@ -1,12 +1,15 @@
 from http.server import BaseHTTPRequestHandler
-from cowpy import cow
-
+from urllib import parse
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
+        s = self.path
+        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
+        if "name" in dic:
+            message = "Hello, " + dic["name"] + "!"
+        else:
+            message = "Hello, stranger!"
         self.wfile.write(message.encode())
         return
