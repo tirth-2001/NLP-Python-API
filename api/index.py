53cbd37c6393f93f9c5cf89e1ea31f29df26acfc
tirth-2001 <tirthgpatel.27@gmail.com>
2021-09-07 14:11:55 +0530

    Python function

--- api/index.py
+++ api/index.py
@@ -1,15 +1,41 @@
+from html.parser import HTMLParser
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+
+
+def getdata():
+    parser = HTMLParser()
+    parser.feed("""<!DOCTYPE html>
+<html>
+<head>
+<title>HTML Title</title>
+</head>
+<body>
+<h1>Header 1</h1>
+<h2>Header 2</h2>
+<h3>Header 3</h3>
+<p>This is a paragraph.</p>
+</body>
+</html>""")
+    return parser.get_starttag_text()
+
+
+if __name__ == "__main__":
+    print(getdata())
+
+
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         s = self.path
         dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
+        tag = getdata()
+        print(tag)
         if "name" in dic:
             message = "Hello, " + dic["name"] + "!"
         else:
             message = "Hello, stranger!"
-        self.wfile.write(message.encode())
+        self.wfile.write(message.encode(), tag.encode())
         return
